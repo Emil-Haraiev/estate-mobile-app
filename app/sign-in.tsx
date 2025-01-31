efa079f884656ae3df8da8a1e15edda931815370
Emil <e.garaev.dg@gmail.com>
2025-01-31 21:29:38 +0200

    Authentication

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,27 @@
-import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native'
+import {View, Text, ScrollView, Image, TouchableOpacity, Alert} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import {login} from "@/lib/appwrite";
+import { Redirect } from "expo-router";
+import {useGlobalContext} from "@/lib/global-provider";
+
 
 const SignIn = () => {
+    const { refetch, loading, isLogged } = useGlobalContext();
 
-    const handleLogin = () => {}
+    if (!loading && isLogged) return <Redirect href="/" />;
+
+    const handleLogin = async () => {
+        const result = await login();
+
+        if(result){
+            refetch();
+        } else {
+            Alert.alert("Error", "Failed")
+        }
+    }
     return (
         <SafeAreaView className="bg-white h-full">
             <ScrollView contentContainerClassName="h-full">
