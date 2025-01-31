ef24ddb4e4a5ecb0ece0d90bfb0146169d163fb0
Emil <e.garaev.dg@gmail.com>
2025-02-01 01:19:27 +0200

    Profile section & tabs

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View , Image} from "react-native";
 import  {Link} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import images from '@/constants/images'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Text className="font-rubik text-lg my-10">Home</Text>
-        <Link href="/sign-in">Sign in</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
-    </View>
+   <SafeAreaView className='bg-white h-full'>
+       <View className="px-5">
+           <View className="flex flex-row items-center justify-between mt-5">
+               <View className="flex flex-row items-center">
+                   <Image source={images.avatar} className="size-12 rounded-full" />
+                   <View className="flex flex-row items-start ml-2 justify-center">
+
+                   </View>
+               </View>
+           </View>
+       </View>
+   </SafeAreaView>
   );
 }
